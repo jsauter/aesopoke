3d2ad45c62d8e382984cab5a8ccdf761d08e400c
Jon Sauter <jsauter@gmail.com>
2018-07-24 23:45:51 -0600

    creating pip dependencies

--- aesopoke.py
+++ aesopoke.py
@@ -1,3 +1,11 @@
 import urllib2
+from lxml import etree
+
 content = urllib2.urlopen("http://ets.aeso.ca/ets_web/ip/Market/Reports/SMPriceReportServlet?contentType=html").read()
-print content
+
+table = etree.HTML(s).find("body/table")
+rows = iter(table)
+headers = [col.text for col in next(rows)]
+for row in rows:
+    values = [col.text for col in row]
+    print dict(zip(headers, values))
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Aesopoke',
+      version='1.0',
+      description='Get Data from Aeso',
+      author='Jonathan Sauter',
+      author_email='jsauter@gmail.com',
+     )
